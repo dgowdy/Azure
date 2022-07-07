2ac85f2ee35b452b35fe47292a2c1f97cf5bd159
David Gowdy <34044429+dgowdy@users.noreply.github.com>
2022-07-07 15:19:56 -0400

    Create Set-Profile.ps1

---
+++ Set-Profile.ps1
@@ -0,0 +1,10 @@
+<# 
+# Execute the following line to persist the file in your profile:
+(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/dgowdy/Azure/master/Set-Profile.ps1') | 
+  Set-Content $profile -Force
+#>
+
+$profilePath = 'https://raw.githubusercontent.com/dgowdy/Azure/master/profile.ps1'
+
+$downloadString = '{0}?{1}' -f $profilePath, (New-Guid)
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($profilePath))
